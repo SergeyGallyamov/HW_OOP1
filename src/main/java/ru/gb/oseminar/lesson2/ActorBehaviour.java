92e85418cd45e03fe4cc8dc0c911455ef06a61fc
Sergey Gallyamov <sergeycronk385@mail.ru>
2024-09-15 00:37:59 +0400

    Домашняя работа по 2 уроку

---
+++ src/main/java/ru/gb/oseminar/lesson2/ActorBehaviour.java
@@ -0,0 +1,8 @@
+package ru.gb.oseminar.lesson2;
+
+public interface ActorBehaviour {
+    void setMakeOrder(boolean makeOrder);
+    void setTakeOrder(boolean pickUpOrder);
+    boolean isMakeOrder();
+    boolean isTakeOrder();
+}
